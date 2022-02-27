0638fbd66bbc2edc0a1dc027ba1e87710faef5cf
dasusekhar <dasusekhar4130@gmail.com>
2022-02-27 17:39:51 +0530

    add git-demo project githubs to  local repository

---
+++ src/main/java/GitHubs/gitHubs/App.java
@@ -0,0 +1,19 @@
+package GitHubs.gitHubs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public void addData(String data)
+	{
+		System.out.println(" data "+data);
+	}
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
